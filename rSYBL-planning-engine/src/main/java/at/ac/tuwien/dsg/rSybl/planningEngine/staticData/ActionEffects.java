2530417f747b7f6f99d09faf1f2b0fa722b74e33
Georgiana Copil <e.copil@dsg.tuwien.ac.at>
2013-10-03 16:46:54 +0200

    modified effects

--- rSYBL-planning-engine/src/main/java/at/ac/tuwien/dsg/rSybl/planningEngine/staticData/ActionEffects.java
+++ rSYBL-planning-engine/src/main/java/at/ac/tuwien/dsg/rSybl/planningEngine/staticData/ActionEffects.java
@@ -80,7 +80,7 @@ public class ActionEffects {
 
 		{
 			scaleInEffectForCassandraDB.setTargetedEntityID("CassandraNode");
-			scaleInEffectForCassandraDB.setActionEffectForMetric("cpuUsage", 40.0f,"CassandraNode");
+			scaleInEffectForCassandraDB.setActionEffectForMetric("cpuUsage", 35.0f,"CassandraNode");
 //			scaleInEffectForCassandraDB.setActionEffectForMetric("latency", 0.0001f,"DataNodeServiceUnit");
 //			scaleInEffectForCassandraDB.setActionEffectForMetric("cost", -0.12f,"DataNodeServiceUnit");
 			scaleInEffectForCassandraDB.setActionName("scaleInEffectForDataNode");
