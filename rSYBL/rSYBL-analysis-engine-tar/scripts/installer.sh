e5e94d91c63a9f88724f3c3726b641c94a8789b4
Daniel Moldovan <d.moldovan@dsg.tuwien.ac.at>
2015-09-15 13:31:01 +0200

    updated scripts for decision module tar modules

--- rSYBL/rSYBL-analysis-engine-tar/scripts/installer.sh
+++ rSYBL/rSYBL-analysis-engine-tar/scripts/installer.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+ 
+DEC_MODULE_DIR=/opt/decision-module
 
-CATALINA_HOME=${CATALINA_HOME?"CATALINA_HOME should be set."}
+mkdir -p $DEC_MODULE_DIR
+cp ./*jar $DEC_MODULE_DIR
+cp -R ./config $DEC_MODULE_DIR
+cp ./config.properties $DEC_MODULE_DIR
 
-cp war/* $CATALINA_HOME/webapps/
+cp ./decision-module /etc/init.d/
+chmod +x /etc/init.d/decision-module
+update-rc.d -f decision-module defaults
+
+
