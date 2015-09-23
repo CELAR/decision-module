9bb845726b1a1e5e9e04e4114c4927ab16077707
Daniel Moldovan <d.moldovan@dsg.tuwien.ac.at>
2015-09-23 15:20:36 +0200

    updated dependency to solve missing class issue

--- rSYBL/rSYBL-cloud-application-dependency-graph/src/main/java/at/ac/tuwien/dsg/csdg/outputProcessing/celar/OutputProcessing.java
+++ rSYBL/rSYBL-cloud-application-dependency-graph/src/main/java/at/ac/tuwien/dsg/csdg/outputProcessing/celar/OutputProcessing.java
@@ -7,34 +7,32 @@ package at.ac.tuwien.dsg.csdg.outputProcessing.celar;
 import at.ac.tuwien.csdg.utils.celar.Configuration;
 import at.ac.tuwien.dsg.csdg.Node;
 import at.ac.tuwien.dsg.csdg.elasticityInformation.ElasticityCapability;
+import at.ac.tuwien.dsg.csdg.inputProcessing.multiLevelModel.deploymentDescription.DeploymentDescription;
 import at.ac.tuwien.dsg.csdg.outputProcessing.OutputProcessingInterface;
 
-import at.ac.tuwien.dsg.csdg.utils.DependencyGraphLogger;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Date;
 import java.util.HashMap;
-import java.util.Map.Entry;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
  * @author Georgiana
  */
 public class OutputProcessing implements OutputProcessingInterface {
-    
-    public static String API_URL="http://localhost:8080/celar-orchestrator/deployment/";
-	
-    public OutputProcessing(){
-     	  API_URL=Configuration.getCELARManagerURL();
-		
-		 
+
+    public static String API_URL = "http://localhost:8080/celar-orchestrator/deployment/";
+
+    static {
+//        ensure that the cloud DeploymentDescription is injected in the generated jar
     }
+
+    public OutputProcessing() {
+        API_URL = Configuration.getCELARManagerURL();
+        System.out.println("DeploymentDescription loaded as " + DeploymentDescription.class.getName());
+    }
+
     @Override
-    public void saveActionPlan(HashMap<Node,ElasticityCapability> actionPlan) {
+    public void saveActionPlan(HashMap<Node, ElasticityCapability> actionPlan) {
      //TODO: save action plan in celar data base through celar manager
-        
+
     }
-    
+
 }
